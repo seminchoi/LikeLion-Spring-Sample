ee87a2e731118771b4a658a30061c89a8a269e38
guitarcode <choicco89@gmail.com>
2023-05-02 13:09:57 +0900

    스프링 2주차 샘플코드

---
+++ src/main/java/com/test/SpringBootApi/domain/Product.java
@@ -0,0 +1,32 @@
+package com.test.SpringBootApi.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "product_name")
+    private String productName;
+
+    @Column(name = "price")
+    private int price;
+
+    public Product(String productName, int price) {
+        this.productName = productName;
+        this.price = price;
+    }
+
+}
---
+++ src/main/java/com/test/SpringBootApi/respository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.test.SpringBootApi.respository;
+
+import com.test.SpringBootApi.domain.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
